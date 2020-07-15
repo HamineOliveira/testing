6788f82dce53861fe0b82e6a2a86cee15e58eca1
HamineOliveira <hamine.oliveira@unesp.br>
2020-07-15 16:26:31 -0300

    Inicando R

---
+++ Aprendendo R.R
@@ -0,0 +1,16 @@
+##Começando do básico
+x <- 1 ##Comentario
+print(x)
+y <- 1:20
+y
+z <- 1/Inf ##Inf é infinito
+z
+
+##Criando vetores
+v1<- c(1.3,2.1,3.7) ##numeric
+v2<- c(TRUE, TRUE, FALSE)  ##logical
+v3<- c(T,F,F)  ##logical
+v4<- c("Oi", "Bye", "Hello")  ##character
+v5<- c(1, 2, 5)  ##integer
+v6<- c(1+2i, 2i, 5+3i)  ##complex
+h<-vector("numeric", length=5)
